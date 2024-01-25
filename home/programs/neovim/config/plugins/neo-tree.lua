7e9d3c4ac4caa5dfa917ddabaf5eee2114fe4a9d
Franck Hu <byfranckhu@outlook.com>
2024-01-25 14:21:27 +0100

    fix(nvim): neo-tree

--- home/programs/neovim/config/plugins/neo-tree.lua
+++ home/programs/neovim/config/plugins/neo-tree.lua
@@ -1,4 +1,9 @@
 require("neo-tree").setup({
+  filesystem = {
+    bind_to_cwd = false,
+    follow_current_file = { enable = true },
+    use_libuv_file_watcher = true,
+  },
   default_component_configs = {
     indent = {
       with_expanders = true,
