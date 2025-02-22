ed5f13eb42f46f030f72ea7fbbf01ba78d7a0c00
Helyo <byhelyo@gmail.com>
2025-02-22 18:14:47 +0100

    fix(nvidia): crash on sleep/suspend

--- hosts/module/hardware/nvidia.nix
+++ hosts/module/hardware/nvidia.nix
@@ -7,6 +7,8 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
