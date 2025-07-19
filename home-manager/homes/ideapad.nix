9191bbb462d335e1f60e208a52209f606ab457e8
Helyo <byhelyo@gmail.com>
2025-07-19 04:44:00 +0200

    fix(ideapad): pkgs: cli

--- home-manager/homes/ideapad.nix
+++ home-manager/homes/ideapad.nix
@@ -26,7 +26,7 @@ in
     homeDirectory = "/home/helyo";
 
     packages = with pkgs; [ ]
-      ++ (import ../pkgs/cli.nix { inherit pkgs; })
+      ++ (import ../pkgs/cli.nix { inherit pkgs config lib; })
       ++ (import ../pkgs/js.nix { inherit pkgs; })
       ++ (import ../pkgs/nix-tools.nix { inherit pkgs; })
       ++ (import ../pkgs/rust.nix { inherit pkgs fenix; })
